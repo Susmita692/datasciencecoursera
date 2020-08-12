cdf47ef849750ab4594dd491664625725b74a41c
Susmita692 <66878196+Susmita692@users.noreply.github.com>
2020-08-12 22:52:32 +0530

    Add files via upload

---
+++ cache.R
@@ -0,0 +1,24 @@
+makeCacheMatrix<- function(x =matrix()){
+  
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  get<-function() {x}
+  setinv<-function(inverse) {inv<<-inverse}
+  getinv<-function(){inv}
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+}
+
+cachesolve<- function(x,...){
+  inv<-x$getinv()
+  if(!is.null(inv)){
+    message("need cache data")
+    return(inv)
+  }
+  mat<-x$get()
+  inv<-solve(mat,...)
+  x$setinv(inv)
+  inv
+}
